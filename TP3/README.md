cc030c5fc0dc4fe4bc3b5966632596d5a2345fbf
“NunoRodriguess” <130775733+NunoRodriguess@users.noreply.github.com>
2024-02-26 19:11:09 +0000

    tp3

---
+++ TP3/README.md
@@ -0,0 +1,5 @@
+# Somador on/off
+
+Autor: Jorge Nuno Gomes Rodrigues,A101758
+
+O meu objetivo foi traduzir como funciona um autómato, isto é, pensar nas suas transições tendo em conta o seu estado atual e a nova palavra que acaba de receber. Para esse efeito, filtrei tudo o que não importava para o automato, ficando apenas com os **On**, **Off**, **=** e os **inteiros**.Este processo foi feito linha a linha.De seguida apenas atravessei esta lista realizando as operações necessárias.
---
+++ TP3/somador_er.py
@@ -0,0 +1,31 @@
+import sys
+import re
+
+current_state = 'OFF'
+lista_numeros = []
+
+for line in sys.stdin:
+
+    tokens = re.finditer(r'OFF|ON|=|[+\-]?\d+', line,re.IGNORECASE)
+    for token in tokens:
+        if current_state == 'OFF':
+            if token.group().upper() == 'ON':
+                current_state = 'ON'
+            elif token.group().upper() == 'OFF':
+                current_state = 'OFF'
+            elif token.group().upper() == '=':
+                print(sum(map(int,lista_numeros)))
+            else:
+                pass
+            
+        elif current_state == 'ON':
+            
+            if token.group().upper() == 'ON':
+                current_state = 'ON'
+            elif token.group().upper() == 'OFF':
+                current_state = 'OFF'
+            elif token.group().upper() == '=':
+                print(sum(map(int,lista_numeros)))
+            else:
+                lista_numeros.append(token.group())
+
